4bcc1d21163f8119b5db7f0ca815c373c23a0787
mae <110599787+mar19999@users.noreply.github.com>
2026-03-16 16:13:41 +0800

    Initial commit: Create grid and apply hover effect

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < 16*16; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    });
+
+}
