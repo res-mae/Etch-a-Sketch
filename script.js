b050a5ea81302fede98b033f8e1155bfb3021a62
mae <110599787+mar19999@users.noreply.github.com>
2026-03-17 22:06:32 +0800

    added option to ressize grid

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
+let gridSize = 16;
+
 const grid = document.querySelector(".grid");
 
-for (let i = 0; i < 16*16; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    grid.appendChild(div);
+const input = document.querySelector("#size-input");
+const resizeButton = document.querySelector(".resize-button");
 
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "black";
-    });
+// resize the grid layout
+resizeButton.addEventListener("click", () => {
+    gridSize = input.value;
 
+    if(gridSize > 100 || gridSize === "") {
+        return;
+    }
+
+    grid.innerHTML = "";
+    createGrid();
+})
+
+// function to create grid
+function createGrid() {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+
+        const squareWidth = 100 / gridSize;
+
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        squareDiv.style.width = `${squareWidth}%`;
+        squareDiv.style.height = `${squareWidth}%`;
+
+        grid.appendChild(squareDiv);
+
+        squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style.backgroundColor = "black";
+        });
+    }
 }
+
+// initial grid
+createGrid();
